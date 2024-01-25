1d20ae01fd3a996c8f11e3a7afe6b0f39f2ab484
Eto Vass <eto.vass.art@gmail.com>
2024-01-25 22:52:31 +0200

    adding deploy-sepolia.sh

---
+++ tutorial-2-NFT-contract/deploy-sepolia.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export PRIVATE_KEY=<YOUR_PRIVATE_KEY>
+export NODE_URL=https://ethereum-sepolia.publicnode.com
+export ETHERSCAN_API_KEY=<YOUR_ETHERSCAN_API_KEY>
+
+forge script script/Deploy.s.sol:Deploy --fork-url $NODE_URL --broadcast --verify -vvvv
